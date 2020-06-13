ebcc6225e8c1086f29039d7a9705d9ae3774ac62
stuart-p <stuart_palmer85@hotmail.com>
2020-06-13 15:14:36 +0100

    oauth flow working

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -25,29 +25,51 @@ exports.loginCallback = (req, res, next) => {
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  console.log(code, state, storedState);
   if (state === null || state !== storedState) {
     res.redirect("/#" + querystring.stringify({ error: "state_mismatch" }));
   } else {
     res.clearCookie(stateKey);
-    const tokenRequestUrl= "https://accounts.spotify.com/api/token",
-    const authOptions = {
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: "authorization_code",
-      }};
-     const header ={ 
-       headers: {
-        Authorization:
-          "Basic " +
-          new Buffer(client_id + ":" + client_secret).toString("base64"),
-      }
+    const tokenRequestUrl = "https://accounts.spotify.com/api/token";
+    const authOptions = querystring.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirect_uri,
+    });
+    const header = {
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
     };
 
-    return axios.post(tokenRequestUrl,authOptions, header).then((err, res)=>{
-console.log(res)
+    return axios({
+      method: "post",
+      url: tokenRequestUrl,
+      data: authOptions,
+      headers: header,
     })
+      .then(({ data }) => {
+        const access_token = data.access_token;
+        const refresh_token = data.refresh_token;
+
+        // just test it works
+        const profileURL = "https://api.spotify.com/v1/me";
+        const header = {
+          headers: {
+            Authorization: "Bearer " + access_token,
+          },
+        };
+
+        return axios.get(profileURL, header);
+      })
+      .then((data) => {
+        console.log("DATA RECEIVED FROM SPOTIFY!!!!=====================>");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("ERROR==========================>");
+        console.log(err);
+      });
   }
 };
 
